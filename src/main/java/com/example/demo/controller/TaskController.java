8a6070ad576fb7e9f26e44018fecba1397aaec9f
heverton100 <heverton.rc.100@gmail.com>
2022-08-09 15:52:03 -0300

    Initial commit

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import com.example.demo.entity.TaskEntity;
 import com.example.demo.repository.TaskRepository;
 
 @Controller
-@RequestMapping(path = "/task")
+@RequestMapping(path = "/")
 public class TaskController {
 
     @Autowired
@@ -38,20 +39,83 @@ public class TaskController {
 
         task.setDescription(description);
         task.setCategory(category);
-        task.setStatus(null);
+        task.setStatus(0);
 
         taskRepository.save(task);
         
-        return "redirect:/task/list-task/";
+        return "redirect:/";
     }
 
-    @RequestMapping("/list-task")
+    @RequestMapping("/")
     public String list_task(Model model) {
 
         model.addAttribute("taskList", taskRepository.findAll());
 
         return "list_task";
         
+    }
+
+    @GetMapping("/edit-task/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        TaskEntity task = taskRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid task Id:" + id));
+        
+        model.addAttribute("description", task.getDescription());
+        model.addAttribute("id", id);
+        model.addAttribute("category", task.getCategory());
+
+        return "edit_task";
+    }
+
+    @PostMapping("/update-task/{id}")
+    public String updateTask(@PathVariable("id") long id, @RequestParam String description, @RequestParam String category, Model model) {
+
+        TaskEntity task = taskRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid task Id:" + id));
+
+        task.setDescription(description);
+        task.setCategory(category);
+            
+        taskRepository.save(task);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/check-task/{id}")
+    public String checkTask(@PathVariable("id") long id, Model model) {
+
+        TaskEntity task = taskRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid task Id:" + id));
+
+        task.setStatus(1);
+            
+        taskRepository.save(task);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/uncheck-task/{id}")
+    public String uncheckTask(@PathVariable("id") long id, Model model) {
+
+        TaskEntity task = taskRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid task Id:" + id));
+
+        task.setStatus(0);
+            
+        taskRepository.save(task);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete-task/{id}")
+    public String deleteTask(@PathVariable("id") long id, Model model) {
+
+        TaskEntity task = taskRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid task Id:" + id));
+            
+        taskRepository.delete(task);
+
+        return "redirect:/";
     }
 
 }
